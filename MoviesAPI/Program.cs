56cc23f7a6b447d29e8f397e4ff0967fc7ad4cd5
Dorijan Ivanković <doivanko@vsite.hr>
2025-01-26 17:47:46 +0100

    valjda zadnja

--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -3,10 +3,11 @@ using MoviesAPI.Data;
 using MoviesAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<AppDbContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnString")
- ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
+
+// Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -16,6 +17,7 @@ builder.Services.AddScoped<MoviesService>();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -29,3 +31,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
